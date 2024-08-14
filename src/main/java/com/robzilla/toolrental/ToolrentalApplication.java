6fdf9a5ecfd88e5d513a100fcfa4c533381fe985
Robert Moore <rob@robzilla.com>
2024-08-14 12:50:53 -0400

    Add Spring Boot application.

---
+++ src/main/java/com/robzilla/toolrental/ToolrentalApplication.java
@@ -0,0 +1,13 @@
+package com.robzilla.toolrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolrentalApplication.class, args);
+	}
+
+}
